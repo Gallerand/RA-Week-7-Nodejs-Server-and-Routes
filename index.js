b4dac7eb2b5d402c9ddc55e2e00dc9db8659d53f
Gallerand <115129377+Gallerand@users.noreply.github.com>
2023-03-12 15:34:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,13 @@ const server = http
     const url = req.url;
     let tableData =
       "<table border='1'><tr><th>Name</th><th>Height</th><th>Birth Year</th><th>Gender</th><th>URL</th></tr>";
+
     if (url === "/") {
       res.write("<h1>Welcome to the Home Page</h1>");
       res.end(
         '<img src="https://dummyimage.com/600x400/2e71db/ded41f&text=Hello!">'
       );
-    }
-    if (url === "/errorpage") {
-      res.write("Error Page");
-      res.end();
-    }
-    if (url === "/list") {
+    } else if (url === "/list") {
       fetch("https://swapi.dev/api/people")
         .then((res) => res.json())
         .then((data) => {
@@ -24,6 +20,10 @@ const server = http
           res.write(tableData);
           res.end();
         });
+    } else {
+      url === "";
+      res.write("Error 404: Page Not Found");
+      res.end();
     }
 
     function createData(data) {
